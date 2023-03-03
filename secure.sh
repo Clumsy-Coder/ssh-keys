68c582cf664a35a6115a20dc6698d3cf7f02b43d
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-03 11:58:55 -0700

    feat(bash): add script to decrypt and extract contents from the encrypted tarball
      ## what
      - add script to decrypt the encrypted tarball
      - update docs
    
      ## how
      - using `gpg` to decrypt the encrypted tarball
      - using `tar` to extract contents from the tarball
      - using `sha256sum` to check the integrity of files
      - using `shred` to remove files
      - decrypting the tarball
          - check if encrypted file doesn't exist
          - exit program
        - check integrity of encrypted tarball
        - decrypt the encrypted tarball
        - check integrity of the tarball
        - extract contents of the tarball in the current directory
        - check integrity of ssh keys
        - remove tarball
        - exit program
    
      ## why
      - this will be needed when decrypting the tarball
      - this will be used when setting up a new machine
    
      ## where
      - ./secure.sh
    
      ## usage

--- secure.sh
+++ secure.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Usage:
+# - to encrypt ssh keys
+# ./secure.sh
+#
+###################################################################################################
+# - to descrypt ssh keys
+# ./secure.sh -d
+#
+# or
+#
+# ./secure.sh --decrypt
+###################################################################################################
+###################################################################################################
+###################################################################################################
+# TLDR decrypting SSH keys
+# - check if encryption file doesn't exist
+#   - exit program
+# - check file integrity of encrypted tarball
+# - decrypt the encrypted tarball
+# - check file integrity of tarball
+# - extract tarball contents in the current directory
+# - check integrity of ssh keys
+# - remove tarball
+# - exit program
+
 # TLDR encrypting SSH keys
 # - generate sha256sum for ssh keys to a file
 # - print sha256sum of ssh keys
@@ -16,6 +41,42 @@ ENCRYPTED_TARBALL_NAME="$TARBALL_NAME".encrypted
 SHA256SUM_FILE=sha256sum.txt
 SSH_KEY_SHA256SUM_FILE=ssh-keys-checksum.txt
 ###################################################################################################
+
+###################################################################################################
+###################################### DECRYPT SSH KEYS ###########################################
+###################################################################################################
+if [ "$1" == "-d" ] || [ "$1" == "--decrypt" ]; then
+  if [ ! -f "$ENCRYPTED_TARBALL_NAME" ]; then
+    echo "> Encrypted file $ENCRYPTED_TARBALL_NAME doesn't exist"
+    exit 1
+  fi
+
+  echo "> Checking integrity of $ENCRYPTED_TARBALL_NAME"
+  sha256sum --check "$SHA256SUM_FILE" --ignore-missing
+  echo ""
+
+  echo "> Decrypting $ENCRYPTED_TARBALL_NAME"
+  gpg --decrypt --no-symkey-cache --output "$TARBALL_NAME" "$ENCRYPTED_TARBALL_NAME"
+  echo ""
+
+  echo "> Checing integrity of $TARBALL_NAME"
+  sha256sum --check "$SHA256SUM_FILE" --ignore-missing
+  echo ""
+
+  echo "> Extracting tarball contents"
+  tar xf "$TARBALL_NAME"
+  echo ""
+
+  echo "> Checking integrity of ssh keys"
+  sha256sum --check "$SSH_KEY_SHA256SUM_FILE"
+  echo ""
+
+  echo "> Removing $TARBALL_NAME"
+  shred --remove "$TARBALL_NAME"
+
+  exit
+fi
+
 ###################################################################################################
 ###################################### ENCRYPT SSH KEYS ###########################################
 ###################################################################################################
