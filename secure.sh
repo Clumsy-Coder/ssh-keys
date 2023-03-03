8d210db9f728109cd650cf0568a004d25680f128
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-03 11:52:44 -0700

    feat(bash): generate sha256sum for ssh keys before creating tarball
      ## what
      - create sha256sum for ssh keys
        - use a new variable to name the ssh keys sha256sum file
        - generate sha256sum for ssh keys into a file
      - update docs
      - remove
        - tarball
        - sha256sum of ssh keys
    
      ## how
    
      ## why
      - this is to check if the extracted files from the tarball are good
    
      ## where
      - ./secure.sh
    
      ## usage

--- secure.sh
+++ secure.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# TLDR
-# - create a tarball to ~/.ssh
+# TLDR encrypting SSH keys
+# - generate sha256sum for ssh keys to a file
+# - print sha256sum of ssh keys
+# - create a tarball of ssh keys to ~/.ssh
 # - print sha256sum of the tarball
-# - add sha256sum of the tarball to a checksum file
+# - generate sha256sum of the tarball to a file
 # - encrypt tarball
 # - print sha256sum of the encrypted tarball
-# - add sha256sum of the encrypted tarball to checksum file
+# - generate sha256sum of the encrypted tarball to a file
 ###################################################################################################
 
 TARBALL_NAME=secrets.tar.gz
 ENCRYPTED_TARBALL_NAME="$TARBALL_NAME".encrypted
 SHA256SUM_FILE=sha256sum.txt
+SSH_KEY_SHA256SUM_FILE=ssh-keys-checksum.txt
+###################################################################################################
+###################################################################################################
+###################################### ENCRYPT SSH KEYS ###########################################
 ###################################################################################################
 # remove existing tarball, encrypted tarball and sha256sum.txt
 
@@ -19,14 +25,37 @@ echo "> Removing $TARBALL_NAME"
 shred --remove "$TARBALL_NAME" 2> /dev/null
 echo "> Removing $ENCRYPTED_TARBALL_NAME"
 shred --remove "$ENCRYPTED_TARBALL_NAME" 2> /dev/null
-echo "> Removing sha256sum.txt"
+echo "> Removing $SHA256SUM_FILE"
 shred --remove $SHA256SUM_FILE 2> /dev/null
+echo "> Removing $SSH_KEY_SHA256SUM_FILE"
+shred --remove $SSH_KEY_SHA256SUM_FILE 2> /dev/null
 
 ###################################################################################################
-# create an TARBALL_NAME
+# generate sha256sum file for ssh keys
+echo "Generating sha256sum for ssh keys into file $SSH_KEY_SHA256SUM_FILE"
 
+# obtained from
+#   https://unix.stackexchange.com/a/468747
+#   https://askubuntu.com/a/1091369
+find . \( -exec [ -f {}/.git ] \; -prune \) -o \( -name .git -prune \) -o \
+! -name "." \
+! -name .gitignore \
+! -name README.md \
+! -name allowed_signers \
+! -name "*.sh" \
+! -name known_hosts \
+! -name sha256sum.txt \
+! -name ssh-keys-checksum.txt \
+! -name "*.tar.*" \
+-exec sha256sum {} \; >> $SSH_KEY_SHA256SUM_FILE
+
+echo "sha256sum for ssh keys"
+cat $SSH_KEY_SHA256SUM_FILE
+echo ""
+
+###################################################################################################
+# create a tarball
 printf "> Creating Tarball\n\n"
-
 # obtained from https://stackoverflow.com/a/3035446
 tar -czvf "$TARBALL_NAME" -C ~/.ssh \
   --exclude .git/ \
@@ -56,5 +85,10 @@ echo "> sha256sum of encrypted tarball $ENCRYPTED_TARBALL_NAME"
 sha256sum "$ENCRYPTED_TARBALL_NAME"
 sha256sum "$ENCRYPTED_TARBALL_NAME" >> $SHA256SUM_FILE
 echo ""
+
+###################################################################################################
+# remove tarball
+shred --remove $TARBALL_NAME
+shred --remove $SSH_KEY_SHA256SUM_FILE
 
 ###################################################################################################
